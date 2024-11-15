5597f5bb662dddb8f095aa76ce95c57ded0199c4
MarcoAntonioDFGava <marcogava192425@gmail.com>
2024-11-15 18:40:01 -0300

    Continuação scripts
    Continuação da programação do script do Pardalberto, finalizando o dia.

--- Impulse Music/Assets/Scripts/scriptpardal.cs
+++ Impulse Music/Assets/Scripts/scriptpardal.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum velocity { Slow = 0, Normal = 1, Fast = 2, Faster = 3, Fastest = 4 };
+public enum Velocity { Slow = 0, Normal = 1, Fast = 2, Faster = 3, Fastest = 4 };
 
 public class Movement : MonoBehaviour
 {
-    public velocity CurrentSpeeds;
+    public Velocity CurrentSpeeds;
     //			    0	   1	  2	  3 	 4
     float[] velocityValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
 
